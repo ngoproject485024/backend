fea88b09c2f869f20d8f7c0490c465072ba33033
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-03-15 22:19:50 +0330

    add username password to ngos

--- ngo-project/src/ngo/ngo.module.ts
+++ ngo-project/src/ngo/ngo.module.ts
@@ -5,12 +5,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ngoSchema } from './entities/ngo.entity';
 import { documentSchema } from './entities/document.entity';
 import { projectSchema } from './entities/project.entity';
+import { jwtService } from 'src/jwt/jwt.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports : [MongooseModule.forRoot('') , 
-      MongooseModule.forFeature([{name : 'ngo' , schema : ngoSchema} , {name : 'document' , schema : documentSchema} , {name : 'project' , schema : projectSchema}])
+      MongooseModule.forFeature([{name : 'ngo' , schema : ngoSchema} , {name : 'document' , schema : documentSchema} , {name : 'project' , schema : projectSchema}]),
+      JwtModule.registerAsync({
+          imports: [ConfigModule],
+          useFactory: async (configService: ConfigService) => ({
+            secret: process.env.JWT_SECRET,
+          }),
+        }),
   ],
   controllers: [NgoController],
-  providers: [NgoService],
+  providers: [NgoService , jwtService],
 })
 export class NgoModule {}
