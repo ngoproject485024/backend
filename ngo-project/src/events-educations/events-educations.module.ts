2b8bf87a1fa261f824ea7772a492b1457ac06162
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-01 11:11:11 +0330

    adf

--- ngo-project/src/events-educations/events-educations.module.ts
+++ ngo-project/src/events-educations/events-educations.module.ts
@@ -6,9 +6,20 @@ import { EducationSchema } from './entities/education.entity';
 import { EventsSchema } from './entities/events.entity';
 import { adminAuth } from 'src/admin-auth/admin-auth.middleware';
 import { jwtService } from 'src/jwt/jwt.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports:[MongooseModule.forFeature([{name : 'educations' , schema : EducationSchema} , {name : 'events' , schema : EventsSchema}])],
+  imports:[MongooseModule.forFeature([{name : 'educations' , schema : EducationSchema} , {name : 'events' , schema : EventsSchema}]),
+
+JwtModule.registerAsync({
+    imports: [ConfigModule],
+    useFactory: async (configService: ConfigService) => ({
+      // secret: process.env.JWT_SECRET,
+      global : true
+    }),
+  })
+],
   controllers: [EventsEducationsController],
   providers: [EventsEducationsService , jwtService],
 })
