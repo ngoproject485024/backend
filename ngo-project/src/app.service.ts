5da67dd54878ed3b1f5c82f0a50d503b1d186cee
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-07 00:19:12 +0330

    adff

--- ngo-project/src/app.service.ts
+++ ngo-project/src/app.service.ts
@@ -115,7 +115,7 @@ export class AppService {
 
   async specificProjectsByStatus(req: any, res: any, status: string , page : number) {
     // let projects = await this.projectRepository.find({ status: {$in : status} })
-    await this.projectRepository.findOneAndUpdate({name : 'bbbb'} , {status : ['goodPractice']})
+    // await this.projectRepository.findOneAndUpdate({name : 'bbbb'} , {status : ['goodPractice']})
     let projects = await this.projectRepository.find().populate({path : 'ngo' , select : {'_id' : 1 , 'name' : 1 , 'username' : 1 , 'city': 1 , 'countrye' : 1 , 'nationalId' : 1}}) 
     return {
       message: 'get all projects page data by status',
