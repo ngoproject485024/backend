e3fbc05c65bec307a3e8efbb5e70e17eaf40224c
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-04-11 15:30:45 +0330

    add home page

--- ngo-project/src/dto/homePage.dto.ts
+++ ngo-project/src/dto/homePage.dto.ts
@@ -4,7 +4,7 @@ import { IsObject, IsString } from "class-validator";
 
 
 export class homePage{
-    @IsNotEmpty()
+
     @IsArray()
     @ApiProperty({
         example : [],
@@ -13,112 +13,112 @@ export class homePage{
     mainImages: string[]
     
     
-    @IsNotEmpty()
+
     @IsArray()
     @ApiProperty({
         example : [],
         required : true
     })
     middleImages: string[]
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     peDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     peMiddleImageDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     peProjectDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     peAboutUsDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     peNgoDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     enDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     enMiddleImageDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     enProjectDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     enAboutUsDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     enNgoDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     ruDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     ruMiddleImageDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     ruProjectDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
         required : true
     })
     ruAboutUsDescription: string
-    @IsNotEmpty()
+
     @IsString()
     @ApiProperty({
         example : '',
