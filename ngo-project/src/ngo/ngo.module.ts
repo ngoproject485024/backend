6fb2faef707d58df9f5c3900f690ebb52853196c
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-16 16:09:04 +0330

    adf

--- ngo-project/src/ngo/ngo.module.ts
+++ ngo-project/src/ngo/ngo.module.ts
@@ -39,7 +39,7 @@ export class NgoModule implements NestModule {
       { path: '/ngo/project/update/:id', method: RequestMethod.POST },
       { path: '/ngo/project/ongoing/:id', method: RequestMethod.POST },
       { path: '/ngo/project/delete/:id', method: RequestMethod.POST },
-      { path: '/ngo/project/complete', method: RequestMethod.POST },
+      { path: '/ngo/project/complete/:projectId', method: RequestMethod.POST },
       { path: '/ngo/pannel/documents', method: RequestMethod.GET },
       { path: '/ngo/token/check', method: RequestMethod.GET },
       { path: '/ngo/pannel/projects', method: RequestMethod.GET })
