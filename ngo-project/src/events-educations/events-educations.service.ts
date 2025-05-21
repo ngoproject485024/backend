dadf9da035358e1075d53cb0f221b51f8f0878a9
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-22 00:12:45 +0330

    asdf

--- ngo-project/src/events-educations/events-educations.service.ts
+++ ngo-project/src/events-educations/events-educations.service.ts
@@ -170,10 +170,10 @@ export class EventsEducationsService {
       if (!isNaN(+type)) {
         console.log('first condition22')
         // let mainType = +type == 1 ? 'Education' : (+type == 2) ? "Youth" :  (+type == 3) ? "Women" : "Climate Change"
-        event = await this.eventRepository.find({ type: +type }).sort({ 'createdAt': -1 }).limit(10).skip(+page * 10)
+        event = await this.eventRepository.find({ type: +type }).sort({ 'createdAt': -1 }).limit(10).skip(((+page)-1) * 10)
       }
       console.log('first condition3')
-      event = await this.eventRepository.find().sort({ 'createdAt': -1 }).limit(10).skip(+page-1 * 10)
+      event = await this.eventRepository.find().sort({ 'createdAt': -1 }).limit(10).skip(((+page)-1) * 10)
     } else {
       console.log('first condition44')
       page = "0"
