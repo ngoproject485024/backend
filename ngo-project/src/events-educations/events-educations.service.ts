92a47ca7b30476427734b944afdd9c3ac1476f26
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-22 00:05:10 +0330

    asdf

--- ngo-project/src/events-educations/events-educations.service.ts
+++ ngo-project/src/events-educations/events-educations.service.ts
@@ -158,15 +158,16 @@ export class EventsEducationsService {
    * @returns 
    */
   async getAllEvents(req: any, res: any, type: string, sort: string, start: string, end: string , page : string) {
-    console.log(typeof(sort) !== undefined)
-    console.log(typeof(type) === undefined)
+    console.log(sort)
+    console.log(type)
     console.log(start)
     console.log(end)
+
     // type=2&start=2025-5-8&end=2025-5-22&page=2
     let event;
-    if (!!page) {
+    if (!isNaN(+page)){
       console.log('first condition')
-      if (!!type) {
+      if (!isNaN(+type)) {
         console.log('first condition22')
         // let mainType = +type == 1 ? 'Education' : (+type == 2) ? "Youth" :  (+type == 3) ? "Women" : "Climate Change"
         event = await this.eventRepository.find({ type: +type }).sort({ 'createdAt': -1 }).limit(10).skip(+page * 10)
