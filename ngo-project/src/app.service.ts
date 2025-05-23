7d6afda86b01f6c426df3431ddcf45b63323ad64
hosseinkhodakhah6@gmail.com <hosseinkhodakhah>
2025-05-23 14:40:41 +0330

    asdf

--- ngo-project/src/app.service.ts
+++ ngo-project/src/app.service.ts
@@ -490,7 +490,7 @@ export class AppService {
       .populate({ path: 'ngo', select: { '_id': 1, 'name': 1, 'username': 1, 'city': 1, 'countrye': 1, 'nationalId': 1, 'logo': 1 } })
       .limit(10).skip((+page-1)*10)
     }
-    let all = await this.projectRepository.countDocuments()
+    let all = await this.projectRepository.countDocuments({ status: { $in: status } })
     // await this.projectRepository.findOneAndUpdate({name : 'bbbb'} , {status : ['goodPractice']})
     // let projects = await this.projectRepository.find().populate({ path: 'ngo', select: { '_id': 1, 'name': 1, 'username': 1, 'city': 1, 'countrye': 1, 'nationalId': 1, 'logo': 1 } })
     return {
